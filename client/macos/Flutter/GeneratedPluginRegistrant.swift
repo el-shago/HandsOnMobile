bca69f5dad37e7ed3b74f96d3a48eac1f0281ccc
Leonardo Luna <93144721+leolunax@users.noreply.github.com>
2024-05-21 08:45:14 -0700

    eliminar auth0

--- client/macos/Flutter/GeneratedPluginRegistrant.swift
+++ client/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,8 +5,6 @@
 import FlutterMacOS
 import Foundation
 
-import auth0_flutter
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
-  Auth0FlutterPlugin.register(with: registry.registrar(forPlugin: "Auth0FlutterPlugin"))
 }
